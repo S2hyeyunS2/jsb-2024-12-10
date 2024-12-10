6d249c09caf94f826abcca745f48ce068eeff8d8
S2hyeyunS2 <hhyeyun22@gmail.com>
2024-12-10 10:48:16 +0900

    ResponseBody

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -2,20 +2,24 @@ package com.mysite.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller //스프링에서는 Controller로 취급이 된다
 public class MainController {
     //Get http://localhost:8080/
     @GetMapping("/")
-    public void home() {
+    @ResponseBody //리턴한 값을 고객에게 보여준다
+    public int home() {
         System.out.println("Home");
-        this.contact();
+        return 22;
     }
 
     //Get http://localhost:8080/about
     @GetMapping("/about")
-    public void about() {
+    @ResponseBody
+    public String about() {
         System.out.println("about");
+        return "안녕하세요.";
     }
 
     public void contact(){
